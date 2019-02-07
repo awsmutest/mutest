d835889d8e851446ff09cad71f67dac674513288
martin <martin.siitmaa@zlick.it>
2019-02-07 11:16:56 +0200

    para

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ var server = app.listen(3000, function() {
 module.exports = server;
 
 
-function getUrlParameter(param1) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + param1 + '=([^&#]*)');
-  var results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  console.log(param1);
-};
+// function getUrlParameter(param1) {
+//  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+//  var regex = new RegExp('[\\?&]' + param1 + '=([^&#]*)');
+//  var results = regex.exec(location.search);
+//  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+//  console.log(param1);
+//};
 //
