33caab43afe954011ce7d070018f06c5a65f4951
martin <martin.siitmaa@zlick.it>
2019-03-06 09:30:36 +0200

    .env

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 var fs = require('fs');
 var parameter = fs.readFileSync('.env','utf8');
 var code_hash = fs.readFileSync('code_hash.txt','utf8');
 console.log (code_hash);
-console.log (parameter);
+console.log ('parameter on:', parameter);
 console.log('The IPADDRESS is:', process.env.IP);
 console.log('The message is:', process.env.AZ);
 console.log('The hash is: %s', code_hash);
