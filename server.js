8cdb7b3e139681c1fa0310b7fe8359495685afa6
martin <martin.siitmaa@zlick.it>
2019-03-29 08:38:29 +0200

    var parameter

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path')
 var fs = require('fs');
 
 console.log('change')
-// var parameterr = fs.readFileSync('.env','utf8');
+var parameterr = fs.readFileSync('.env','utf8');
 // var code_hash = fs.readFileSync('code_hash.txt','utf8');
 // console.log (code_hash);
 console.log(path.resolve(process.cwd(), '.env'))
